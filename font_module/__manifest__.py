0838c9fb1241a5c49ddda6133ec6f3999ecb31b0
Grupo-Vitta <matias.artesi@grupoattain.com.ar>
2024-11-07 12:46:31 -0300

    Add files via upload

---
+++ font_module/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Fuentes Personalizadas',
+    'version': '1.0',
+    'depends': ['web'],
+    'data': [],
+    'assets': {
+    'web.assets_backend': [
+        'font_module/static/src/css/custom_fonts.css',
+    ],
+    'web.assets_frontend': [
+        'font_module/static/src/css/custom_fonts.css',
+    ],
+    },
+    'installable': True,
+    'auto_install': False,
+ 
+}
